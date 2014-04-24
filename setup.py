d2f1b243f1cbf7f099bc37578e1b30853c350e7a
Jarvys <yuhan534@126.com>
2014-04-25 00:18:50 +0800

    add homepage's url

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from distutils.core import setup
 
 setup(
     name='pymist',
-    version='0.0.1',
+    version='0.0.2',
     description='Python port of node\'s minimist module',
     author='Jarvys',
     author_email='yuhan534@126.com',
+	url="https://github.com/jarvys/pymist",
     py_modules=['pymist'],
 )
